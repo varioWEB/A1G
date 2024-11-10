b85b94c458e05497e920ebd268b7b37abf690938
varioWEB <operational.vario@gmail.com>
2024-11-10 20:35:02 +0200

    edit compTest

--- compTest.tsx
+++ compTest.tsx
@@ -3,7 +3,7 @@ import Rive from '@rive-app/react-canvas';
 function compTest(props?: {text?: string}){
   return(
     <Rive
-    src="./assets/vehicles.riv"
+    src="vehicles.riv"
     stateMachines={props?.text}
   />
   );
